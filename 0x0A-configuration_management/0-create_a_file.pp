204d7bbfcd635835d4f3c47b305a90cdc6c08075
abrahamof2016 <jarageda9@gmail.com>
2024-04-01 08:38:50 +0000

    create a file with puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # create a file in /tmp.
 file { '/tmp/school':
-    ensure => present,
+    ensure  => present,
     content => 'I love puppet',
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
 }
