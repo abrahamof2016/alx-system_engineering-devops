e2eb1262040ff482f267a067352a37e3e2e1c845
abrahamof2016 <jarageda9@gmail.com>
2024-07-31 13:24:44 +0000

    Install Nginx web server (w/ Puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,23 @@
+# Install and configure an Nginx server using puppet instead of
+# Bash.
+# You should also include resources in your manifest to perform a 301 redirect
+# with querying /redirect_me
+package { 'nginx':
+ensure => installed,
+}
+
+file_line { 'aaaaa':
+ensure => 'present',
+path => '/etc/nginx/sites-available/default',
+after => 'listen 80 default_server;',
+line  => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.html':
+content => 'Hello World!',
+}
+
+service { 'nginx':
+ensure => running,
+require => package['nginx'],
+}
