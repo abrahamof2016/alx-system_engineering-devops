076bcda4b57c1b213798d03666effd4e2d41530f
abrahamof2016 <jarageda9@gmail.com>
2024-07-31 13:51:52 +0000

    Install Nginx web server (w/ Puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -8,9 +8,9 @@ ensure => installed,
 
 file_line { 'aaaaa':
 ensure => 'present',
-path => '/etc/nginx/sites-available/default',
-after => 'listen 80 default_server;',
-line  => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+path   => '/etc/nginx/sites-available/default',
+after  => 'listen 80 default_server;',
+line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.html':
@@ -18,6 +18,6 @@ content => 'Hello World!',
 }
 
 service { 'nginx':
-ensure => running,
+ensure  => running,
 require => package['nginx'],
 }
